1fb9d5d5da92e562ec878228325dce71a038e60b
Jeppe <jebar@student.sdu.dk>
2022-12-17 13:30:52 +0100

    I made a bunch of changes to the post handler and also altered the client side js, so that instead of storing an action in localStorage, it sends the action to the server.

--- TesterSelenium.py
+++ TesterSelenium.py
@@ -4,8 +4,11 @@ from selenium.webdriver.common.action_chains import ActionChains
 import http.server
 import socketserver
 import json
-import re
 import time
+from pathlib import Path
+
+
+filename = 'sites.txt'
 
 class Action:
     def __init__(self, type, xpath, value):
@@ -21,6 +24,58 @@ class Site:
 		self.url = url
 		self.actions = actions
 
+def default(o):
+	if isinstance(o, Action):
+		return {
+			'action_type': o.action_type,
+			'xpath': o.xpath,
+			'value': o.value
+		}
+	elif isinstance(o, Site):
+		obj = {
+			"url":o.url,
+			"actions":[]
+		}
+		for a in o.actions:
+			obj["actions"].append({
+				'action_type': a.type,
+				'xpath': a.xpath,
+				'value': a.value
+			})
+		return obj
+	else:
+		raise TypeError(f'Object of type {o.__class__.__name__} is not JSON serializable')
+
+def write_to_file(sites):
+    # Open the file in write mode
+    with open(filename, 'w') as f:
+        # Convert the sites list to a JSON string
+        data = json.dumps(sites, default=default)
+        # Write the JSON string to the file
+        f.write(data)
+
+def object_hook(d):
+    if 'actions' in d:
+        return Site(d['url'], [Action(a['action_type'], a['xpath'], a['value']) for a in d['actions']])
+    return d
+
+# Read the sites and actions from a text file
+def read_from_file():
+	myfile = Path(filename)
+	myfile.touch(exist_ok=True)
+
+	# Open the file in read mode
+	with open(filename, 'r') as f:
+		# Read the contents of the file
+		data = f.read()
+		if data == '':
+			return []
+			
+		# Convert the JSON string to a list of sites
+		sites = json.loads(data, object_hook=object_hook)
+		return sites
+
+
 class MyServer(http.server.SimpleHTTPRequestHandler):
 	def do_GET(self):
 		print(self.path)
@@ -31,12 +86,31 @@ class MyServer(http.server.SimpleHTTPRequestHandler):
 		# Read the request body
 		content_length = int(self.headers['Content-Length'])
 		body = self.rfile.read(content_length)
-
+		print("inside post handler")
 		# Unpack the JSON object from the request body
 		data = json.loads(body)
 
-		# Do something with the data here...
 		print(data)
+
+		sites = read_from_file()
+
+		foundSite = False
+		for s in sites:
+			if(s.url == data["site"]):
+				foundSite = True
+				lastAction = s.actions[len(s.actions) - 1]
+				if(data["action_type"] == "keypress" and lastAction.type == "keypress" and lastAction.xpath == data["xpath"]):
+					lastAction.value = lastAction.value + data["value"]	
+				else:
+					s.actions.append(Action(type=data["action_type"], xpath=data["xpath"], value=data["value"]))
+				
+				break;
+		
+		if foundSite is False:
+			newSite = Site(url=data["site"], actions=[Action(type=data["action_type"], xpath=data["xpath"], value=data["value"])])
+			sites.append(newSite)
+
+		write_to_file(sites)
 
 		# Send a response
 		self.send_response(200)
@@ -52,24 +126,6 @@ class MyServer(http.server.SimpleHTTPRequestHandler):
 	def do_OPTIONS(self):
 		self.send_response(200)
 		self.end_headers()
-
-def write_to_file(sites):
-    # Open the file in write mode
-    with open('sites.txt', 'w') as f:
-        # Convert the sites list to a JSON string
-        data = json.dumps(sites, default=lambda o: o.__dict__)
-        # Write the JSON string to the file
-        f.write(data)
-
-# Read the sites and actions from a text file
-def read_from_file():
-    # Open the file in read mode
-    with open('sites.txt', 'r') as f:
-        # Read the contents of the file
-        data = f.read()
-        # Convert the JSON string to a list of sites
-        sites = json.loads(data, object_hook=lambda d: Site(**d))
-        return sites
 
 def launchServerForPersistence():
 	handler_object = MyServer
@@ -87,50 +143,6 @@ def launchServerForPersistence():
 		print('Closing')
 		my_server.server_close()
 
-def default(o):
-	if isinstance(o, Action):
-		return {
-			'action_type': o.action_type,
-			'xpath': o.xpath,
-			'value': o.value
-		}
-	elif isinstance(o, Site):
-		obj = {
-			"url":o.url,
-			"actions":[]
-		}
-		for a in o.actions:
-			obj["actions"].append({
-				'action_type': a.action_type,
-				'xpath': a.xpath,
-				'value': a.value
-			})
-		return obj
-	else:
-		raise TypeError(f'Object of type {o.__class__.__name__} is not JSON serializable')
-
-def write_to_file(sites):
-    # Open the file in write mode
-    with open('sites.txt', 'w') as f:
-        # Convert the sites list to a JSON string
-        data = json.dumps(sites, default=default)
-        # Write the JSON string to the file
-        f.write(data)
-
-def object_hook(d):
-    if 'actions' in d:
-        return Site(d['url'], [Action(a['action_type'], a['xpath'], a['value']) for a in d['actions']])
-    return d
-
-# Read the sites and actions from a text file
-def read_from_file():
-    # Open the file in read mode
-    with open('sites.txt', 'r') as f:
-        # Read the contents of the file
-        data = f.read()
-        # Convert the JSON string to a list of sites
-        sites = json.loads(data, object_hook=object_hook)
-        return sites
 
 def parseActions(actionsJson):
 	jsonObjs = []
@@ -176,6 +188,6 @@ def recordSiteTemplate(URL):
 	driver.execute_script(script)
 	time.sleep(100)
 
-recordSiteTemplate("https://www.billetlugen.dk/")
-#launchServerForPersistence()
+# recordSiteTemplate("https://www.billetlugen.dk/")
+launchServerForPersistence()
 #performActions()
